c42bd9765edefd35fc6069baa0ddad4aee736c3c
jwang12345 <jwang1@expedia.com>
2017-10-26 11:28:29 -0700

    check train on batch

--- distkeras/workers.py
+++ distkeras/workers.py
@@ -632,11 +632,11 @@ class ADAGWorkerWithDistributedParameterServer(NetworkWorker):
         print("""before optimize""" + str(self.get_worker_id()))
         while True:
             X, Y = self.get_next_minibatch()
-            h = self.model.train_on_batch(X, Y)
-            self.add_history(h)
-            sys.stderr.write("Worker_id: " + str(self.worker_id) + " Epoch: " + str(self.current_epoch) + " Iteration: " + str(self.iteration) + " loss:" + str(h) + "\n")
-            sys.stderr.write("Worker_id: " + str(self.worker_id) + " Epoch: " + str(self.current_epoch) + " Iteration: " + str(self.iteration) + " avg_loss:" + str(self.cacul_avg_loss(h,self.iteration)) + "\n")
-            sys.stderr.flush()
+            #h = self.model.train_on_batch(X, Y)
+            #self.add_history(h)
+            #sys.stderr.write("Worker_id: " + str(self.worker_id) + " Epoch: " + str(self.current_epoch) + " Iteration: " + str(self.iteration) + " loss:" + str(h) + "\n")
+            #sys.stderr.write("Worker_id: " + str(self.worker_id) + " Epoch: " + str(self.current_epoch) + " Iteration: " + str(self.iteration) + " avg_loss:" + str(self.cacul_avg_loss(h,self.iteration)) + "\n")
+            #sys.stderr.flush()
             if self.iteration % self.communication_window_executor == 0:
                 W2 = np.asarray(self.model.get_weights())
                 delta = W2 - W1
